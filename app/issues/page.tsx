d1bd833d7bcefd27a37862d9bdd69150a9174eb2
Matthew Fuss <mfuss@opticooltech.com>
2024-02-28 12:43:38 -0500

    build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
-import { Button, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
+import Link from "../components/Link";
 import React from "react";
-import Link from "next/link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import delay from "delay";
