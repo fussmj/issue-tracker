de4fb647804430ba2c5f215cb57c8d538805afee
Matthew Fuss <mfuss@opticooltech.com>
2024-04-03 11:50:21 -0400

    add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,8 @@ import Link from "next/link";
 import { AiFillBug } from "react-icons/ai";
 import { usePathname } from "next/navigation";
 import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box } from "@radix-ui/themes";
 
 const NavBar = () => {
   const links = [
@@ -12,6 +14,7 @@ const NavBar = () => {
     { label: "Issues", href: "/issues" },
   ];
   const currenPath = usePathname();
+  const { status, data: session } = useSession();
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
@@ -19,20 +22,29 @@ const NavBar = () => {
         <AiFillBug />
       </Link>
       <ul className="flex space-x-6">
-        {links.map((link) => (
-          <Link
-            key={link.href}
-            className={classNames({
-              "text-zinc-900": link.href === currenPath,
-              "text-zinc-500": link.href !== currenPath,
-              "hover:text-zinc-800 transition-colors": true,
-            })}
-            href={link.href}
-          >
-            {link.label}
-          </Link>
+        {links.map((link, index) => (
+          <li key={index}>
+            <Link
+              className={classNames({
+                "text-zinc-900": link.href === currenPath,
+                "text-zinc-500": link.href !== currenPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log Out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Log In</Link>
+        )}
+      </Box>
     </nav>
   );
 };
