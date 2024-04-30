eb6e59da59e667c00887c461fe19b72d4d9ec2c6
Matthew Fuss <mfuss@opticooltech.com>
2024-04-30 15:55:38 -0400

    complete the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@ import { Button, Flex } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
 import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 import LatestIssues from "./api/LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({
+export default async function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return <LatestIssues />;
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
