5391a8fc69db55fea384105a046f071652f6a8bc
Matthew Fuss <mfuss@opticooltech.com>
2024-04-30 14:38:44 -0400

    build latest issues component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 import { Button, Flex } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
 import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
+import LatestIssues from "./api/LatestIssues";
 
 export default function Home({
   searchParams,
 }: {
   searchParams: { page: string };
 }) {
-  return (
-    <Pagination
-      itemCount={99}
-      itemsPerPage={10}
-      currentPage={parseInt(searchParams.page)}
-    />
-  );
+  return <LatestIssues />;
 }
